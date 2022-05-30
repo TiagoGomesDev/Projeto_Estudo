cc73eca97438a4a243d1ceacfa397f2daf9830de
Tiago Gomes <tiago.santos@unidompedro.com>
2022-05-30 16:32:02 -0300

    alteração das imgs test

--- products.js
+++ products.js
@@ -2,7 +2,8 @@ const products = [
   {
     id: 'rec43w3ipXvP28vog',
     title: 'python',
-    company: 'ikea',
+    company: 'front',
+    // company: 'ikea',
     image:
     //   'https://dl.airtable.com/.attachments/14ac9e946e1a02eb9ce7d632c83f742f/4fd98e64/product-1.jpeg',
     // price: 9.99,
