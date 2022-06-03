a81b34105b4c3c7cfea3f51a970c5f7e3db1029c
Tiago Gomes <tiago.santos@unidompedro.com>
2022-06-03 16:07:44 -0300

    telas de cont

--- products.js
+++ products.js
@@ -3,7 +3,7 @@ const products = [
     id: 'rec43w3ipXvP28vog',
     title: 'python',
     company: 'front',
-    href: 'https://www.google.com.br/',
+    href: 'Python.html',
     // company: 'ikea',
     image:
     //   'https://dl.airtable.com/.attachments/14ac9e946e1a02eb9ce7d632c83f742f/4fd98e64/product-1.jpeg',
