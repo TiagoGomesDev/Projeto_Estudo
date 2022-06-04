814236abfc5c8aaadc982c88bdb34b3ee1fd826b
TiagoGomesDev <tiago.santos@unidompedro.com>
2022-06-04 10:10:12 -0300

    Enter

---
+++ js/python.js
@@ -0,0 +1,15 @@
+const products = [
+    {
+      id: 'rec43w3ipXvP28vog',
+      title: 'python',
+      company: 'front',
+      href: 'Python.html',
+      // company: 'ikea',
+      image:
+      //   'https://dl.airtable.com/.attachments/14ac9e946e1a02eb9ce7d632c83f742f/4fd98e64/product-1.jpeg',
+      // price: 9.99,
+      'img/Logo/python.png',
+    },
+  
+  ];
+  
