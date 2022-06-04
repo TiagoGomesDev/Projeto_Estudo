dfc4a6ea2073e808d6b3f1fdcaa2e6777bbf6b13
TiagoGomesDev <tiago.santos@unidompedro.com>
2022-06-04 11:06:00 -0300

    tela python

--- js/python.js
+++ js/python.js
@@ -2,13 +2,15 @@ const products = [
     {
       id: 'rec43w3ipXvP28vog',
       title: 'python',
+      subtitle:'Introdução',
       company: 'front',
       href: 'Python.html',
       // company: 'ikea',
       image:
       //   'https://dl.airtable.com/.attachments/14ac9e946e1a02eb9ce7d632c83f742f/4fd98e64/product-1.jpeg',
       // price: 9.99,
-      'img/Logo/python.png',
+      'img/Logo/pp.jpg',
+      // 'thumb-1920-679140.png';
     },
   
   ];
